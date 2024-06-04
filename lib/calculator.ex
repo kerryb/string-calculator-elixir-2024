7dc16c1a6d9173bad72b18504c255a18870e09f0
Kerry Buckley <kerryjbuckley@gmail.com>
2024-06-04 14:28:35 +0100

    Handle two numbers, but returns extra ‘.0’ with integers

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -2,6 +2,11 @@ defmodule StringCalculator.Calculator do
   def add(""), do: "0"
 
   def add(input) do
-    input
+    input |> String.split(",") |> Enum.map(&to_float/1) |> Enum.sum() |> to_string()
+  end
+
+  defp to_float(string) do
+    {float, _} = Float.parse(string)
+    float
   end
 end
