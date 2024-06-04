3b035c07c37fe044550fb80d6bda80d181817210
Kerry Buckley <kerryjbuckley@gmail.com>
2024-06-04 14:47:39 +0100

    Return an {:ok, result} tuple

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,14 +1,15 @@
 defmodule StringCalculator.Calculator do
-  def add(""), do: "0"
+  def add(""), do: {:ok, "0"}
 
   def add(input) do
-    input
-    |> String.split(~r/[,\n]/)
-    |> Enum.map(&to_float/1)
-    |> Enum.sum()
-    |> Float.round(8)
-    |> to_string()
-    |> String.replace_suffix(".0", "")
+    {:ok,
+     input
+     |> String.split(~r/[,\n]/)
+     |> Enum.map(&to_float/1)
+     |> Enum.sum()
+     |> Float.round(8)
+     |> to_string()
+     |> String.replace_suffix(".0", "")}
   end
 
   defp to_float(string) do
