7c8aff7ce0bc3cc4cc07d32f8ae1335e187b2d3e
Kerry Buckley <kerryjbuckley@gmail.com>
2024-06-04 14:15:33 +0100

    Non-compiling test for empty string

---
+++ test/calculator_test.exs
@@ -0,0 +1,10 @@
+defmodule StringCalculator.CalculatorTest do
+  use ExUnit.Case, async: true
+  alias StringCalculator.Calculator
+
+  describe "StringCalculator.Calculator.add/1" do
+    test "returns zero for an empty string" do
+      assert Calculator.add("") == 0
+    end
+  end
+end
