afee18ad7e98e20ee470ec866b17ae87220eff8b
Kerry Buckley <kerryjbuckley@gmail.com>
2024-06-04 14:46:32 +0100

    Change tests to expect an {:ok, result} tuple

--- test/calculator_test.exs
+++ test/calculator_test.exs
@@ -4,27 +4,27 @@ defmodule StringCalculator.CalculatorTest do
 
   describe "StringCalculator.Calculator.add/1" do
     test "returns zero for an empty string" do
-      assert Calculator.add("") == "0"
+      assert Calculator.add("") == {:ok, "0"}
     end
 
     test "returns the number for a single number" do
-      assert Calculator.add("42") == "42"
+      assert Calculator.add("42") == {:ok, "42"}
     end
 
     test "handles single numbers with a decimal point" do
-      assert Calculator.add("4.2") == "4.2"
+      assert Calculator.add("4.2") == {:ok, "4.2"}
     end
 
     test "adds multiple numbers together" do
-      assert Calculator.add("4.2,3,5,10.1") == "22.3"
+      assert Calculator.add("4.2,3,5,10.1") == {:ok, "22.3"}
     end
 
     test "formats the result as an integer if it has no decimal places" do
-      assert Calculator.add("4.2,3,5,10.8") == "23"
+      assert Calculator.add("4.2,3,5,10.8") == {:ok, "23"}
     end
 
     test "allows newlines as separators" do
-      assert Calculator.add("4.2,3,5\n10.8") == "23"
+      assert Calculator.add("4.2,3,5\n10.8") == {:ok, "23"}
     end
   end
 end
