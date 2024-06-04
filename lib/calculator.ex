dab6c995df0304ee5475def896f0426158599715
Kerry Buckley <kerryjbuckley@gmail.com>
2024-06-04 14:58:11 +0100

    Extract calculation to a private function

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -3,22 +3,20 @@ defmodule StringCalculator.Calculator do
 
   def add(input) do
     cond do
-      input == "" ->
-        {:ok, "0"}
-
-      input =~ ~r/#{@separator_pattern}$/ ->
-        {:error, "Number expected but EOF found."}
-
-      true ->
-        {:ok,
-         input
-         |> String.split(~r/#{@separator_pattern}/)
-         |> Enum.map(&to_float/1)
-         |> Enum.sum()
-         |> Float.round(8)
-         |> to_string()
-         |> String.replace_suffix(".0", "")}
+      input == "" -> {:ok, "0"}
+      input =~ ~r/#{@separator_pattern}$/ -> {:error, "Number expected but EOF found."}
+      true -> {:ok, calculate(input)}
     end
+  end
+
+  defp calculate(input) do
+    input
+    |> String.split(~r/#{@separator_pattern}/)
+    |> Enum.map(&to_float/1)
+    |> Enum.sum()
+    |> Float.round(8)
+    |> to_string()
+    |> String.replace_suffix(".0", "")
   end
 
   defp to_float(string) do
